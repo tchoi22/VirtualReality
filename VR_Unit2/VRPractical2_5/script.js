4b6fcfeb8f3438867e28e57437a471d5f04fb890
tchoi22 <theodorec22@nycstudents.net>
2025-11-17 13:32:20 +0000

    feat: Update maze layout and implement Block class for 3D representation
    - Enhanced maze design with a new layout featuring various paths and obstacles.
    - Introduced a Block class to create 3D box objects in the scene based on maze structure.
    - Integrated block creation logic within the maze rendering loop to dynamically generate the environment.
    - Added a grey stone texture for visual representation of blocks in the virtual environment.

--- VR_Unit2/VRPractical2_5/script.js
+++ VR_Unit2/VRPractical2_5/script.js
@@ -11,19 +11,69 @@
 */
 
 let maze = [
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-  "----------------------",
-];
+  "x--xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  "x-----x--------------x--------------x--x-----------x--x-----x-----------x-----------x--x--x",
+  "x--xxxxxxx--xxxx--xxxxxxxxxx--x--xxxx--xxxx--xxxxxxx--x--xxxxxxx--x--xxxx--xxxx--xxxx--x--x",
+  "x--------x--x-----x-----x--x--x-----x--x-----x--x--------x--------x--------x--------------x",
+  "x--xxxx--xxxx--xxxx--xxxx--xxxxxxx--x--x--xxxx--xxxxxxx--xxxxxxxxxxxxxxxx--xxxxxxx--x--x--x",
+  "x--x--x--x-----x--------x--x--x-----x--x-----------------x-----x--x--x-----x--------x--x--x",
+  "x--x--x--x--xxxxxxx--xxxx--x--xxxx--x--xxxxxxx--xxxx--x--xxxx--x--x--xxxxxxxxxx--x--xxxxxxx",
+  "x--x--------x--------x--------x-----------x--------x--x-----------x--x-----x-----x-----x--x",
+  "xxxxxxx--x--xxxx--xxxx--x--xxxxxxxxxxxxx--x--xxxx--x--x--xxxxxxx--x--xxxx--x--xxxxxxx--x--x",
+  "x--------x--x--x--x--x--x-----x-----x--x-----x--x--x--x--x--x--x-----x--x--x-----x--x-----x",
+  "x--xxxx--xxxx--x--x--x--x--x--x--xxxx--xxxxxxx--xxxxxxx--x--x--xxxx--x--x--xxxx--x--xxxxxxx",
+  "x--x--x-----------x-----x--x--x--x--------------x--x--x-----------x-----------------x-----x",
+  "x--x--xxxxxxx--x--xxxx--x--x--x--x--x--xxxxxxx--x--x--xxxx--x--xxxxxxxxxxxxxxxx--xxxx--x--x",
+  "x--------x--x--x-----x--x--x-----x--x--x--------x-----x--x--x--x-----x--------x--------x--x",
+  "x--xxxxxxx--xxxx--xxxx--xxxx--x--x--xxxxxxx--xxxxxxx--x--x--xxxxxxx--xxxxxxx--x--xxxx--xxxx",
+  "x--x-----x-----------------x--x-----x-----x-----x--------------x--x--x--x-----x--x-----x--x",
+  "x--x--xxxx--xxxx--x--xxxxxxxxxxxxxxxx--x--x--xxxxxxxxxxxxx--xxxx--x--x--xxxx--xxxxxxx--x--x",
+  "x--------x--x-----x-----x--------x--x--x--x-----------------x-----x--x--x--------x-----x--x",
+  "x--xxxxxxxxxxxxxxxx--x--x--xxxxxxx--xxxx--x--xxxxxxx--x--xxxx--xxxx--x--x--x--xxxx--x--x--x",
+  "x--------x--------x--x-----x-----x--------x--x--------x--x--------------x--x-----x--x-----x",
+  "x--xxxxxxxxxxxxx--xxxxxxxxxxxxx--xxxxxxx--x--x--x--xxxx--x--xxxxxxx--x--x--xxxx--x--xxxx--x",
+  "x-----x--------x-----x-----x-----------------x--x-----x-----x-----x--x--x--x-----x--x-----x",
+  "x--xxxx--xxxxxxx--x--xxxx--xxxxxxx--x--x--xxxxxxxxxxxxxxxxxxx--xxxxxxxxxxxxx--xxxxxxx--xxxx",
+  "x--x--x--x--x--x--x--------x-----x--x--x--x-----x-----x--------x--------x-----x--------x--x",
+  "x--x--x--x--x--x--xxxx--xxxxxxx--xxxx--xxxx--xxxxxxx--xxxxxxx--xxxxxxx--x--xxxx--xxxxxxx--x",
+  "x--------x--x--------x-----------x--x--x--x-----------x-----x-----x-----x--------------x--x",
+  "xxxx--xxxx--x--xxxx--xxxx--xxxx--x--x--x--xxxxxxx--xxxx--xxxx--xxxx--xxxx--xxxxxxxxxx--x--x",
+  "x--x-----x-----x--x-----x--x-----x--------x-----x--------x-----x-----x--x-----x-----x--x--x",
+  "x--xxxx--xxxx--x--x--x--xxxxxxx--xxxxxxx--x--xxxx--xxxx--x--xxxx--xxxx--xxxx--xxxx--xxxx--x",
+  "x--------x--x--x-----x--x--x-----x--------------x--x--x--x--x--x--x--x--x--------------x--x",
+  "xxxx--xxxx--x--xxxx--x--x--x--xxxxxxx--xxxxxxx--xxxx--x--x--x--x--x--x--x--xxxxxxxxxxxxx--x",
+  "x-----------x-----x--x--x-----x--------x-----------x-----x--x--x-----------x--------x-----x",
+  "xxxxxxx--x--xxxx--x--xxxxxxxxxx--xxxxxxxxxx--x--x--xxxx--x--x--xxxx--x--x--xxxxxxx--x--xxxx",
+  "x-----x--x--x-----x--x--x--x--x-----x-----x--x--x--------x-----------x--x--x-----x--x-----x",
+  "xxxx--x--x--xxxx--xxxx--x--x--xxxxxxx--xxxx--xxxx--x--xxxxxxx--x--xxxx--x--x--xxxx--x--xxxx",
+  "x--x-----x-----------x--x-----------------x--x-----x-----x-----x-----x--x-----------x-----x",
+  "x--x--x--xxxxxxx--x--x--x--xxxx--xxxxxxxxxxxxxxxx--x--xxxxxxxxxx--x--x--xxxx--xxxx--x--xxxx",
+  "x-----x--x--x--x--x--------x-----x--x-----x-----x--x--x--------x--x--x--x-----x--x-----x--x",
+  "x--x--xxxx--x--xxxx--xxxxxxxxxx--x--x--x--x--xxxxxxxxxxxxx--xxxx--xxxxxxx--x--x--x--xxxx--x",
+  "x--x--x-----x--x--------x-----x--x--x--x-----x--------x--------x--x--------x-----x--x-----x",
+  "xxxx--x--x--x--xxxx--xxxxxxx--x--x--xxxx--x--xxxxxxx--xxxxxxx--xxxxxxx--xxxxxxx--xxxx--x--x",
+  "x--------x--x-----x--x--x-----x-----x--x--x--x-----x--x--x--------x-----x--x-----------x--x",
+  "xxxx--x--x--x--x--xxxx--x--x--xxxxxxx--x--xxxx--xxxx--x--x--xxxxxxx--xxxx--x--xxxx--x--xxxx",
+  "x--x--x--x-----x--x-----x--x--------------------------------x--x--x-----x-----x--x--x-----x",
+  "x--xxxxxxxxxxxxxxxxxxx--xxxx--x--xxxxxxxxxx--x--xxxx--x--x--x--x--xxxxxxxxxxxxx--xxxxxxx--x",
+  "x-----------x--------------x--x--x-----x--x--x--x--x--x--x-----------x-----------x--x--x--x",
+  "xxxx--xxxxxxxxxxxxx--x--xxxxxxx--x--x--x--xxxx--x--x--xxxxxxxxxx--xxxxxxx--xxxxxxx--x--x--x",
+  "x--------------------x-----x-----x--x-----x-----x-----x--------x--------x--x--------------x",
+  "x--xxxxxxxxxxxxx--xxxxxxx--x--xxxx--x--xxxxxxxxxxxxx--x--xxxx--xxxx--x--x--x--xxxx--x--xxxx",
+  "x--------x--x--x--x--------x-----x--x-----x-----------x-----x--------x-----------x--x--x--x",
+  "xxxxxxxxxx--x--x--xxxx--xxxx--xxxx--x--x--x--xxxx--xxxx--xxxxxxxxxxxxx--x--xxxx--xxxx--x--x",
+  "x--------x-----------x-----x--x--x--x--x--x-----x--x-----x-----------x--x--x--------x-----x",
+  "xxxx--x--xxxx--xxxxxxx--xxxx--x--x--xxxx--x--x--xxxx--xxxxxxxxxx--x--xxxxxxx--xxxxxxx--x--x",
+  "x-----x-----x--x--------------------x--x--x--x--x-----------------x--x--x-----------x--x--x",
+  "x--xxxx--xxxx--xxxxxxx--x--xxxx--xxxx--xxxx--x--xxxx--x--xxxxxxx--xxxx--x--x--x--x--x--x--x",
+  "x--x--x--x-----------x--x-----x--x--x--------x-----x--x--x--x-----x-----x--x--x--x--x--x--x",
+  "xxxx--x--x--xxxxxxxxxx--xxxxxxx--x--xxxx--xxxxxxx--xxxx--x--xxxx--x--xxxx--x--xxxxxxxxxxxxx",
+  "x--x-----x-----x--x--x--x-----------x--x--x--------x-----------x--------x--x-----x-----x--x",
+  "x--x--x--x--x--x--x--xxxx--x--xxxxxxx--x--xxxxxxx--xxxx--x--x--x--xxxxxxx--x--xxxxxxx--x--x",
+  "x-----x-----x--------x-----x-----------x--x--------x-----x--x--x--x--------x--------------x",
+  "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx--x",
+
+]
 
 /* Challenge 2
    Add appropriate classes to use as objects in your map.  Choose characters to represent these objects and position them on the map.   In Challenge 5 and 6, you will generate the map using the character representation of the objects you chose to place in the world. Get Creative!
@@ -34,6 +84,13 @@ let scene;
 window.addEventListener("DOMContentLoaded",function() {
   scene = document.querySelector("a-scene");
   for(let r = 0; r < maze.length; r++){
+    let row = maze[r];
+    let cols = row.split("");
+    for(let c = 0; c < cols.length; c++){
+      if(cols[c] == "x"){
+        new Block(c,1,r)
+      }
+  }
     /* Challenge 3
       Choose a technique to traverse the each character in the string.
     */ 
